2c18200e48775a1852d76a5434dd31e730f90c3d
onewayay <onewayay@naver.com>
2025-12-04 14:43:13 +0900

    ✨header pc 버전 ui 추가

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,47 +1,34 @@
 import { useState } from 'react';
 import '../../styles/header.css';
-import Menu from './Menu';
+import { Link } from 'react-router-dom';
 
-export default function Header({ qaNum, setQaNum }) {
-  const [isTabOpen, setIsTabOpen] = useState(false); // 탭메뉴 열림/닫힘 상태
+export default function Header() {
+  const [theme, setTheme] = useState('dark');
 
-  // 탭메뉴 열림/닫힘 토글
-  const toggleTabMenu = () => {
-    setIsTabOpen(!isTabOpen);
-    document.body.classList.toggle('fixed');
-  };
-
-  // 탭메뉴 닫기
-  const closeTabMenu = () => {
-    setIsTabOpen(false);
-    document.body.classList.remove('fixed');
+  const changeTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
   };
 
   return (
     <>
       <header id="header">
-        <div className="logo">
-          <h1 id="logo">
-            <a href="/">로고</a>
-          </h1>
+        <div className="inner">
+          <div className="logo">
+            <h1 id="logo">
+              <a href="/">로고</a>
+            </h1>
+          </div>
+          <nav className="nav">
+            <Link to="">문답 보기</Link>
+            <Link to="">주석 모음</Link>
+            <button
+              type="button"
+              className={`theme-btn ${theme}`}
+              onClick={changeTheme}
+            ></button>
+          </nav>
         </div>
-        <button
-          type="button"
-          onClick={toggleTabMenu}
-          className={`tab ${isTabOpen ? 'active' : ''}`}
-        >
-          <i>탭메뉴</i>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
       </header>
-      <Menu
-        qaNum={qaNum}
-        setQaNum={setQaNum}
-        isTabOpen={isTabOpen}
-        closeTabMenu={closeTabMenu}
-      />
     </>
   );
 }
