64bd447d0badddccfe3eda2270fc45a7a4f5c50a
gksrlffla <108395740+onewayay@users.noreply.github.com>
2025-12-14 16:58:51 +0900

    ✨ 페이지 이동시 상단으로 이동되도록 수정(ScrollRestoration)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Footer from './components/layout/Footer';
-import Header from './components/layout/Header';
-import './styles/index.css';
-import { Outlet } from 'react-router-dom';
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import "./styles/index.css";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <ScrollRestoration />
       <Header />
       <main>
         <Outlet />
